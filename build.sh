216dbcef0905a76845ef94acc3a67e9f0394cede
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2024-11-26 20:25:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,3 +30,7 @@ python3.9 manage.py collectstatic --noinput --clear -c
 # Apply migrations (optional)
 echo "Applying database migrations..."
 python3.9 manage.py migrate
+
+
+mkdir -p dist
+touch dist/placeholder.txt
